72aa1131c406fce7f1ef860e4919a8788a4d0678
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2018-12-05 13:29:42 +0100

    Update TimeSheet.cs

--- HHS/TimeRegistrationLibrary/TimeSheet.cs
+++ HHS/TimeRegistrationLibrary/TimeSheet.cs
@@ -5,15 +5,34 @@ namespace TimeRegistrationLibrary
 {
     public class TimeSheet
     {
+        internal class Work
+        {
+            internal KeyValuePair<int, string> workType { get; private set; }
+            internal int Block { get; private set; }
+            internal int Hours { get; private set; }
+
+            public Work(KeyValuePair<int, string> workType, int block = 0, int hours = 0)
+            {
+                this.workType = workType;
+                Block = block;
+                Hours = hours;
+            }
+        }
         
         internal int EmployeeId { get; }
 
-        internal List<KeyValuePair<int, string>> workTypes;
+        internal List<Work> WorkList;
+
 
         public TimeSheet(int employeeId, List<KeyValuePair<int, string>> workTypes)
         {
             EmployeeId = employeeId;
-            this.workTypes = workTypes;
+            foreach (KeyValuePair<int, string> workType in workTypes)
+            {
+                Work workToAdd = new Work(workType);
+                WorkList.Add(workToAdd);
+            }
+           
         }
 
         public void EnterWorkHours(KeyValuePair<int, string> workType, double userInput)
@@ -23,12 +42,12 @@ namespace TimeRegistrationLibrary
 
         public int GetBlockForWorkType(KeyValuePair<int, string> workType)
         {
-            throw new System.NotImplementedException();
+            return WorkList.Find(work => work.workType.Key == workType.Key).Block;
         }
 
-        public object GetHoursRegisteredForWorkType(KeyValuePair<int, string> workType)
+        public int GetHoursRegisteredForWorkType(KeyValuePair<int, string> workType)
         {
-            throw new NotImplementedException();
+            return WorkList.Find(work => work.workType.Key == workType.Key).Hours;
         }
     }
 }
